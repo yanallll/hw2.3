8934018c80666a3abde77b0eec946d439f940182
Yana Shestakova <yana.shestakova.target@gmail.com>
2024-03-13 13:34:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const titleProject = 'Oценка проекта'
+console.log (titleProject)
+
+const screensValue = ("шаблонные, с уникальным дизайном, с анимациями")
+console.log (screensValue)
+
+const screenPrice = 21;
+console.log (screensValue)
+
+const percentage ='40'
+console.log (percentage)
+
+const fullPrice = '500$'
+console.log (fullPrice)
+
+const responsive = 'булевое значение'
+console.log (responsive)
